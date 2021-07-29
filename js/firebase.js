dda79bc7f95ed38001437075b45c75e3465fc747
Glitch (special-wholesale-balmoral) <none>
2021-07-29 22:18:33 +0000

    🚓🎲 Updated with Glitch

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+console.log("js/firebase.js : Remember to replace the instructor's firebase keys with yours!")
+
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDRDSyRzFUnMcIWzIGLCe9VCJgP6JnG-AQ",
+    authDomain: "cssi-day-12-3dd60.firebaseapp.com",
+    projectId: "cssi-day-12-3dd60",
+    storageBucket: "cssi-day-12-3dd60.appspot.com",
+    messagingSenderId: "183257526147",
+    appId: "1:183257526147:web:5a673e9a8f0bea977410b6"
+  };
+// END TODO
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
